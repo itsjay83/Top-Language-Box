e638936ba2a7c426a8d1123608d3678ca9311d24
JayLee_83 <50763770+Suk0803@users.noreply.github.com>
2023-05-13 12:19:56 +0900

    fixed timezone

--- src/app.js
+++ src/app.js
@@ -37,9 +37,9 @@ async function updateGist(username, token, gistId) {
     }
 }
 
-const username = process.env.USER_NAME; // replace with your GitHub username
+const username = process.env.USER_NAME;
 const token = process.env.GH_TOKEN;
-const gistId = process.env.GH_GISTID;// replace with your Gist ID
+const gistId = process.env.GH_GISTID;
 
 updateGist(username, token, gistId);
 
