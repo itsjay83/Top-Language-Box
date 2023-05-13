e5666391b4e2f8bd74aa5179f93dd85ea0933cbc
JayLee_83 <50763770+Suk0803@users.noreply.github.com>
2023-05-13 23:12:37 +0900

    fix app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ const syms = "░▏▎▍▌▋▊▉█";
 const axios = require('axios');
 const calculateLanguageUsage = require('./calculation');
 
+function formatBytes(bytes) {
+    const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    let power = Math.floor(Math.log(bytes) / Math.log(1000));
+    power = Math.min(power, units.length - 1);
+    bytes /= Math.pow(1000, power);
+    return `${bytes.toFixed(2)} ${units[power]}`;
+}
+
 async function updateGist(username, token, gistId) {
     try {
         const { languages, totalBytes } = await calculateLanguageUsage(username, token);
@@ -10,16 +18,23 @@ async function updateGist(username, token, gistId) {
 
         let output = ``;
 
-        // Sort languages by usage in descending order
         const sortedLanguages = Object.entries(languages).sort((a, b) => b[1] - a[1]);
 
+        // Find the length of the longest language name
+        const maxLanguageNameLength = sortedLanguages.reduce((max, [language]) => Math.max(max, language.length), 0);
+        // Find the length of the longest byte string
+        const maxByteLength = sortedLanguages.reduce((max, [_, bytes]) => Math.max(max, formatBytes(bytes).length), 0);
+
         for (const [language, bytes] of sortedLanguages) {
+            const formattedBytes = formatBytes(bytes);
+            const paddedLanguage = language.padEnd(maxLanguageNameLength + 2, " ");
+            const paddedBytes = formattedBytes.padStart(maxByteLength + 2, " ");
             const ratio = bytes / totalBytes;
             const barLength = Math.round(ratio * maxBarLength);
             const fullBlocks = Math.floor(barLength / 8);
             const remainder = barLength % 8;
             const bar = '█'.repeat(fullBlocks) + syms[remainder] + '░'.repeat(maxBarLength / 8 - fullBlocks - 1);
-            output += `${language}: ${bytes} bytes, ${bar} ${(ratio * 100).toFixed(2)}%\n`;
+            output += `${paddedLanguage}${paddedBytes}: ${bar} ${(ratio * 100).toFixed(2)}%\n`;
         }
 
         const gistContent = {
